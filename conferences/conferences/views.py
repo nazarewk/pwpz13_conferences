a085493013c6f04471652217a6b271b59c024b10
Piotr Maj <piotrmaj17@gmail.com>
2013-12-22 18:23:13 +0100

    templates paths refactoring

--- conferences/conferences/views.py
+++ conferences/conferences/views.py
@@ -8,4 +8,4 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 
 def home(request):
-    return render_to_response("home.html", {'data': "Hello there!"})
+    return render_to_response("conferences/home.html", {'data': "Hello there!"})
