b334ef1d04eb39ddfab2634bf0c383c0c110cb68
michael92 <michalsiwek0529@gmail.com>
2014-05-18 16:43:38 +0200

    Add import for decorators

--- conferences/conferences/views.py
+++ conferences/conferences/views.py
@@ -14,6 +14,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from .models import ReviewerForm, SessionForm, TimePeriodForm, LectureForm, UserForm
 from .models import Reviewer, Session, TimePeriod, Lecture, UserProfile, User, Site
