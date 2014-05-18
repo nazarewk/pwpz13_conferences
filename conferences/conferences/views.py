765eea7f8649c6ddf1373a9b55229f319f7c5932
michael92 <michalsiwek0529@gmail.com>
2014-05-18 16:36:11 +0200

    registration/login/logout views

--- conferences/conferences/views.py
+++ conferences/conferences/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.utils.translation import ugettext as _
 from django.shortcuts import render
 
@@ -12,13 +13,30 @@ from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.template import RequestContext
+from django.contrib.auth import authenticate, login, logout
 
-from .models import ReviewerForm, SessionForm, TimePeriodForm, LectureForm
-from .models import Reviewer, Session, TimePeriod, Lecture
+from .models import ReviewerForm, SessionForm, TimePeriodForm, LectureForm, UserForm
+from .models import Reviewer, Session, TimePeriod, Lecture, UserProfile, User, Site
+import hashlib, string, random
 
 
 def home(request):
-    return render(request,"conferences/home.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return render(request, "conferences/home.html")
+            else:
+                # tu gdy zablokowany
+                return render(request, "conferences/home.html")
+        else:
+            # tu gdy nie ma uzytkownika
+            return render(request, "conferences/home.html")
+    else:
+        return render(request, "conferences/home.html")
 
 #reviewers views section
 
@@ -237,3 +255,56 @@ def remove_lecture(request, pk):
     context_dict['lecture_del'] = lecture_del
 
     return render_to_response('conferences/lectures/remove_lecture.html', context_dict, context)
+
+def registration(request):
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.is_active = False
+            user.save()
+            activation_key = hashlib.md5(user.username).hexdigest()
+            activation_key += ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for i in range(8))
+            profile = UserProfile.create(user, activation_key)
+            current_site = Site.objects.get_current()
+            site_url = current_site.domain + "/users/confirm/" + user.username + "/" + activation_key
+            title = "Potwierdzenie rejestracji"
+            content = "Aby dokończyć rejestrację kliknij w link aktywacyjny " + site_url
+            # Nie wiem jak wysłać maila, czy to poleci na podstawie ustawien z django, czy mailem admina, czy jeszcze jak
+            # dlatego send_mail zakomentowane, trzeba poprawic adres a reszta powinna byc ok
+	        # send_mail(title, content, jakis_adres, [user.email], fail_silently=False)
+            profile.save()
+            return redirect('/')
+        else:
+            print user_form.errors
+    else:
+        user_form = UserForm()
+    return render(request, "conferences/users/registration.html", {'form': user_form})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return render(request, "conferences/home.html")
+
+
+def user_confirm(request, username, key):
+    try:
+        user = User.objects.get(username=username)
+        profile = UserProfile.objects.get(user=user)
+        if profile.activation_key==key:
+            user.is_active = True
+            user.save()
+            profile.save()
+            text = "Konto aktywowane. Teraz możesz się zalogować."
+            context = {'message': text}
+            return render(request, 'conferences/users/confirm.html', context)
+        else:
+            text = u'Niepoprawny klucz aktywacyjny.'
+            context = {'message': text}
+            return render(request, 'conferences/users/confirm.html', context)
+    except (User.DoesNotExist, UserProfile.DoesNotExist) as e:
+        text = u'Niepoprawny klucz aktywacyjny.'
+        context = {'message': text}
+        return render(request, 'conferences/users/confirm.html', context)
