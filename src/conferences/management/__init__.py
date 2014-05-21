7cab23bc8948b052af1ea2c466c18f5120bb70e9
Piotr Maj <piotrmaj17@gmail.com>
2014-05-21 23:00:09 +0200

    add initdb script fix

--- src/conferences/management/__init__.py
+++ src/conferences/management/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 __author__ = 'Piotr'
-=======
-__author__ = 'drag0nius'
->>>>>>> 2e15f084726f7b45fcab060490e0e58f865bc161
--- src/conferences/management/commands/conferences_populate.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from conferences.models import Conference
-
-class Command(BaseCommand):
-    args = '<poll_id poll_id ...>'
-    help = 'Closes the specified poll for voting'
-
-    def handle(self, *args, **options):
-        print('Komenda dziala...')
